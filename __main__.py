cb0a166859e18eb8de0381e79543d5a2e124d5e6
Richard Lam <richardlam96@gmail.com>
2020-01-28 14:42:59 -0500

    Renamed notepack2 to notepack since this will replace notepack in the future.

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@
 # Version 2 of the Notepack, which will primarily use Pythopn as the main
 # language instead of bash to allow more functionality for future enhancements.
 #
-from notepack2 import main
+from notepack import main
 import os
 
 
