ec55480102a1d41684df99938ffdeb606d8e976d
U-INDIEPAY\rlam <rlam@GSNYCSLS190031.indiepay.co>
2020-01-29 11:35:44 -0500

    Added logic to log if category and notepack paths exist.

--- notepack/main.py
+++ notepack/main.py
@@ -11,23 +11,43 @@ def notepack():
     output.show_welcome_message()
     initialize_app()
 
-    print("Categories:")
-    show_categories()
+    category = input("Category name: ")
+    notepack = input("Notepack name: ")
+    open_notepack(category, notepack)
+    return
+
+
+def open_notepack(category, notepack):
+    create_category(category)
+    create_notepack(notepack)
+    return
+
+
+def create_category(category):
+    print("Category")
+    category_path = get_category_path(category)
+    if utility.path_exists(category_path):
+        print(f"  {category} already exists")
+    else:
+        print(f"  Creating {category}")
+    return
+
+
+def create_notepack(category, notepack):
+    print("Notepack")
+    notepack_path = get_notepack_path(category, notepack)
+    if utility.path_exists(notepack_path):
+        print(f"  {notepack} already exists in {category}")
+    else:
+        print(f"  Creating {notepack} in {category}")
+    return
 
 
 def show_categories():
     return
 
 
-def create_category(name):
-    return
-
-
 def show_notepacks(category_name):
-    return
-
-
-def create_notepack(name):
     return
 
 
