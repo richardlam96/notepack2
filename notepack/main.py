e1aaec1cfc2328de3bcf1af32b5a788611ba9819
Richard Lam <richardlam96@gmail.com>
2020-02-09 13:57:47 -0500

    Added try/excepts to catch some errors and had console functions return Path objects.

--- notepack/main.py
+++ notepack/main.py
@@ -13,21 +13,25 @@ def notepack():
 
     Includes printing messages and checking for required folders and files.
     """
-    output.print_welcome_message()
-    initialize.confirm_required_folders()
-    initialize.confirm_required_files()
+    try:
+        output.print_welcome_message()
+        initialize.confirm_required_folders()
+        initialize.confirm_required_files()
 
-    # Start the main notepack console.
-    print("What would you like to do?")
-    while True:
-        command = input("> ").split()
-        if not command: continue
+        # Start the main notepack console.
+        print("What would you like to do?")
+        while True:
+            command = input("> ").split()
+            if not command: continue
 
-        action = command[0]
-        if action == 'quit': break
-        if action == 'search': enter_search_console()
+            action = command[0]
+            if action == 'quit': break
+            if action == 'search': enter_search_console()
 
-        continue
+            continue
+    except KeyboardInterrupt:
+        print("Forced exit with keyboard interrupt.")
+
     return
 
 
@@ -43,17 +47,24 @@ def enter_search_console():
         if command[0] == 'quit': break
         
         # Breakdown of command (for clarity).
-        category_name = command[0]
-        notepack_name = command[1]
+        try:
+            category_name = command[0]
+            notepack_name = command[1]
+        except IndexError:
+            print(f"Improper input. Try again.")
+            continue
+
+        # Create the Path objects for each.
         category_path = utility.get_root_path().joinpath(category_name)
         notepack_path = category_path.joinpath(notepack_name)
 
-        # Confirm if you need to create the category and notepack paths.
+        # Confirm if Paths are new and need to be created.
         category_path = confirm_path_console(category_path)
         notepack_path = confirm_path_console(notepack_path)
+        print(f"Category at {category_path}")
+        print(f"Notepack at {notepack_path}")
 
         # Create any missing files and paths for the notepack.
-
     return
 
 
@@ -63,17 +74,18 @@ def confirm_path_console(requested_path):
         # List items in the parent folder to re-choose child.
         print(f"'{requested_path.name}' does not exist.")
         print("Choose existing or create 'new':")
-        '\n'.join(utility.get_path_items(requested_path.parent))
+        print('\n'.join(utility.get_path_items(requested_path.parent)))
         new_path_name = input("existing or 'new'> ")
 
         if new_path_name == 'new':
-            return utility.create_path(requested_path)
+            utility.create_path(requested_path)
+            break
         elif new_path_name == 'quit':
             break
         else:
             requested_path = requested_path.parent.joinpath(new_path_name)
 
-    return
+    return requested_path
 
 
 def pick_from_list(items, prompt="> "):
