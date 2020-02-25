292c406c7c86a090e94395e729a216b9958cdd06
U-INDIEPAY\rlam <rlam@GSNYCSLS190031.indiepay.co>
2020-02-25 15:10:57 -0500

    Added logic to give root_path variable a default path determined by the config to allow flexibility of the create_dir_in_path() function.

--- notepack/utility/path.py
+++ notepack/utility/path.py
@@ -19,6 +19,7 @@ def get_path_items(path):
 
 def create_dir_in_path(root_path, directory_name):
     """Create a new directory in the root path given."""
+    if !root_path: root_path = config.read_root_path()
     new_dir_path = Path(root_path).joinpath(directory_name)
     new_dir_path.mkdir()
     return new_dir_path
@@ -27,5 +28,6 @@ def create_dir_in_path(root_path, directory_name):
 def create_template_copy(root_path, filename):
     """Copy the given file's template to the root path given."""
     return shutil.copy(config.read_template_path(filename), Path(root_path))
+
 
 
